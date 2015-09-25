62687a746ab061173137467e7f841caa920772ed
David English <bavib26@gmail.com>
2015-09-25 08:37:24 -0400

    cleanup

--- MuxSelect/MuxSelect.ino
+++ MuxSelect/MuxSelect.ino
@@ -1,3 +1,4 @@
+// Initialize output GPIOs and Serial port for debug
 void setup()
 {
   Serial.begin(9600);
@@ -7,50 +8,27 @@ void setup()
   pinMode(10, OUTPUT);
 }
 
+// Sample mux_selecting
 void loop()
 {
-  mux_select(5);
+  mux_select(5, 0);
   delay(5000);
-  mux_select(7);
+  mux_select(7, 0);
   delay(5000);
 }
 
-void mux_select(int lane)
+// Activate a mux's lane
+// lane - number 0-15
+// mux - number 0-3, for a 4 mux system
+void mux_select(int lane, int mux)
 {
   int s[4];
-  s[0] = 1 & lane;
-  Serial.print("s0: ");
-  Serial.println(s[0]);
-  if (s[0] != 0){
-    digitalWrite(13,HIGH);
-  } else { 
-    digitalWrite(13,LOW);
-  }
-
-  s[1] = 2 & lane;
-  Serial.print("s1: ");
-  Serial.println(s[1]);
-  if (s[1] != 0){
-    digitalWrite(12,HIGH);
-  } else { 
-    digitalWrite(12,LOW);
-  }
-
-  s[2] = 4 & lane;
-  Serial.print("s2: ");
-  Serial.println(s[2]);
-  if (s[2] != 0){
-    digitalWrite(11,HIGH);
-  } else { 
-    digitalWrite(11,LOW);
-  }
-
-  s[3] = 8 & lane;
-  Serial.print("s3: ");
-  Serial.println(s[3]);
-  if (s[3] != 0){
-    digitalWrite(10,HIGH);
-  } else { 
-    digitalWrite(10,LOW);
+  for(int i = 0; i < 4; i++){
+    s[i] =  pow(2, i) & lane;
+    if (s[i] != 0){
+      digitalWrite(mux * 4 + i,HIGH);
+    } else {
+      digitalWrite(mux * 4 + i,LOW);
+    }
   }
 }
